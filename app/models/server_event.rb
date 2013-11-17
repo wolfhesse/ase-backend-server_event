2a451dfc39394ad8cc893b16d63c7435bbff5701
wolfhesse <wolfgang.schuessel@gmail.com>
2013-11-17 21:16:57 +0100

    wp vim

--- app/models/server_event.rb
+++ app/models/server_event.rb
@@ -2,8 +2,18 @@ class ServerEvent < ActiveRecord::Base
 
   before_save {|e|
     logger.info 'in before_save of ServerEvent '+e.inspect
-    e.vakz = 'initial' if e.vakz.empty?
-    e.va1kz = 'initial' if e.va1kz.empty?
+    e.vakz = 'initial' if e.vakz.empty? unless e.vakz.nil?
+    e.va1kz = 'initial' if e.va1kz.empty? unless e.va1kz.nil?
+
+    # this handles a case with testing: mocked stuff TODO
+    if e.vakz.nil?
+      e.vakz = 'initial'
+    end
+
+    if e.va1kz.nil?
+      e.va1kz = 'initial'
+    end
+
   }
 
   attr_accessible :name, :vakz, :va1kz
